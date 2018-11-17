5b92a573888a26109f9669883218f74b62bd44dc
Evan Laflamme <evanlaflamme@hotmail.com>
2018-11-17 00:28:26 -0500

    Initial welcome UI

---
+++ trackr/ui/TrackrHomeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trackr.ui
+{
+    class TrackrHomeViewModel : ViewModelBase
+    {
+        public event EventHandler Closing;
+
+        public System.Collections.ObjectModel.ObservableCollection<List<string>> strings { get; set; }
+
+        public void Close()
+        {
+            Closing?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
